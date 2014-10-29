885a610cd946ee31db3a4629c91ffbb48c6c3159
Brian /bdougie/ Douglas <brian.douglas@izea.com>
2014-10-28 23:02:45 -0400

    group progress added

--- 03_string_calculator/calculator_test.rb
+++ 03_string_calculator/calculator_test.rb
@@ -1,3 +1,13 @@
 require 'minitest/autorun'
 require_relative "calculator"
 
+class CalculatorTest < MiniTest::Unit::TestCase
+  def test_returns_zero_if_blank
+    assert_equal 0, Calculator.add("")
+  end
+
+  def test_numbers_get_added
+    assert_equal 7, Calculator.add("1,2,4")
+  end
+
+end
