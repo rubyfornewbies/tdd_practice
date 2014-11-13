f6b3be0ae635eeb631add3682b58db272063c496
Brian /bdougie/ Douglas <brian.douglas@izea.com>
2014-11-13 06:56:39 -0500

    solves the problem with more test

--- 04_make_change/change.rb
+++ 04_make_change/change.rb
@@ -1,16 +1,18 @@
 class Coins
+
+  COINSTAR = { :H=>50,:Q=>25,:D=>10,:N=>5,:P=>1 }
+
   def self.make_change(value)
     wallet = {}
-    remainder = 0
     return wallet if value == 0
-    if value < 25
-      quarters = value % 25
-      wallet.merge({:Q=>1})
 
-      value = value - (25 * quarters)
-    else
-      wallet.merge({:P=>1})
+    COINSTAR.each do |k,v|
+      if value >= v
+        wallet[k] = value / v
+        value = value % v
+      end
     end
+    wallet
   end
 
 end
--- 04_make_change/change_spec.rb
+++ 04_make_change/change_spec.rb
@@ -5,17 +5,27 @@ require_relative 'change'
 describe "#Change" do
 
   it "should provide an empty hash if no value is passed in" do
-    assert Coins.make_change(0) == Hash.new
+    assert_equal Hash.new, Coins.make_change(0)
   end
 
   it "should provide a penny back if the value is 1" do
-    assert Coins.make_change(1) == {:P=>1}
+    assert_equal ({:P=>1}), Coins.make_change(1)
   end
 
   it "should provide a penny and quarter if value is 26" do
-    assert Coins.make_change(26) == {:Q=>1,:P=>1}
+    assert_equal ({:Q=>1,:P=>1}), Coins.make_change(26)
+  end
+
+  it "should provide proper change if value is 43" do
+    assert_equal ({:Q=>1, :D=>1, :N=>1, :P=>3}), Coins.make_change(43)
+  end
+
+  it "should provide even more proper change if value is 91" do
+    assert_equal ({:H=>1, :Q=>1, :D=>1, :N=>1, :P=>1}), Coins.make_change(91)
   end
 
 end
+
+
 
 
